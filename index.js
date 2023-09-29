7b2339940d7661082b4efeb5cb04c2d4ee425af0
Techguyak --git <arun02002047@gmail.com>
2023-09-29 23:06:56 +0530

    js added

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const cart = [];
 function add_cart(){
-   
-    let inputel= document.querySelector('.plol');
-    let inpelement= inputel.value;
-     cart.push(inpelement);
-    console.log(cart);  
-    inputel.value='';
-    todolists();
+  let inputel= document.querySelector('.plol');
+  let name= inputel.value;
+
+  let duedate1 = document.querySelector('.duedate');
+  let duedate = duedate1.value;
+    cart.push({
+      name,
+      duedate
+    });
+  console.log(cart);  
+  inputel.value='';
+  duedate1.value='';
+  
+  //    cart.push(duedate2);
+  // duedate1.value = '';   
+  todolists();
 }
 
-
-  function todolists(){
-    
-  let todoli = '';
-
-  for (let i=0;i<=cart.length-1;i++){
-     const todolist = cart[i];
-     console.log(todolist);
-     const html = `<p>${todolist}</p>`;
-     todoli += html;
-  }
-    console.log(todoli);
-    document.querySelector('.panel').
-    innerHTML =todoli;
-  }
+function todolists(){
+let todoli = '';
+for (let i=0;i<=cart.length-1;i++){ 
+    const todolist = cart[i];
+    let {name} = todolist;
+    let {duedate} = todolist
+    console.log(todolist);
+    const html = `
+    <p>${name},${duedate} 
+    <button onclick="
+    cart.splice(${i},1);
+    todolists();
+    ">Delete</button></p>`;
+    todoli += html;
+}
+  console.log(todoli);
+  document.querySelector('.panel').
+  innerHTML =todoli;
+}
